54ab891472529cda2a11ea0534fdfc11fbea5a2b
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-05-07 18:13:41 +0530

    added Suspense fallback

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 import Profile from "@components/Profile";
 
