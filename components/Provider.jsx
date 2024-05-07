b77006c0a2d9645b66ab12fc74866b8fdcb7b395
Shivank Swami <69349438+Shivankswami77@users.noreply.github.com>
2024-05-07 18:29:22 +0530

    added suspense on Provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Suspense } from "react";
 
 const Provider = ({ children, session }) => {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SessionProvider session={session}>{children}</SessionProvider>
+    </Suspense>
+  );
 };
 
 export default Provider;
